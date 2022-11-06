095ed1bade685b92b3eed2af470864b159e79007
Sergey Lushkin <s.lushkin@websecret.by>
2022-11-06 21:29:00 +0300

    refactoring loader

--- src/components/LoaderApp.vue
+++ src/components/LoaderApp.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="wrapper">
-    <img src="@/assets/loader.gif" alt="loader" />
+    <div class="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +25,44 @@
     transform: translate(-50%, -50%);
     width: 100px;
     height: 100px;
+  }
+}
+.lds-ring {
+  display: inline-block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #cef;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #cef transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
